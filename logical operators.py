573e570a2863b0a0cbdf42b78073638f7b5ca9ea
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-29 12:00:27 +0300

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,34 @@
+num1 = 10
+num2 = 20
+print (num1 > num2)
+print (num1 < num2)
+print (10 > 20)
+print (20 ==20)
+print (10 >= 20)
+print (10 <= 20)
+print (10 !=20)
+
+print ( True and True) # True
+print ( True and false)# false
+print ( False and False)# false
+
+w,a = 7,8
+print (w)
+print (a)
+print ( w == w and a == a )
+print ( w == w and a < w)
+
+
+print (True or True) # True
+print ( True or False)# True
+print ( False or False)# false
+
+print ( w == w or a == a )
+print ( w == w or a < w)
+
+
+print ( not True)# false
+print ( not False)# True
+
+print ( not ( w != a))
+print ( not ( w == a))
