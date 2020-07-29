272ecda91d8000e8323e566c4f15f774da594f20
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-29 12:53:38 +0300

    Add files via upload

---
+++ if conditions.py
@@ -0,0 +1,6 @@
+username =  input(" plase insert your name: ")
+password = input(" plase insert your password: ")
+if username == 'bohen' and password == 'ffff':
+    print("welcome to your  emaile")
+else:
+    print ('error')
