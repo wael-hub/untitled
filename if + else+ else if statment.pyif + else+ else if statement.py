5d573d3447dadbd94a95de7a536841fa9ea4f685
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-31 18:58:18 +0300

    Update and rename if conditions.py to if + else+ else if statment.pyif + else+ else if statement.py

---
+++ if + else+ else if statment.pyif + else+ else if statement.py
@@ -0,0 +1,40 @@
+username =  input(" plase insert your name: ")
+password = input(" plase insert your password: ")
+if username == 'bohen' and password == 'ffff':
+    print("welcome to your  emaile")
+else:
+    print ('error')
+    
+    
+    
+z = 15
+y = 10
+if (z < y):
+    print (z)
+    w = z + y
+    print (w)
+elif(z > y):
+    print('iam bohen')
+    w = z - y
+    print(w)
+else:
+    ('x = y')
+
+
+day = 1
+if ( day == 1):
+    print('Sun')
+elif( day == 2):
+    print('Mon')
+elif(day == 3):
+    print('Tues')
+elif(day == 4):
+    print('Wed')
+elif(day == 5):
+    print( 'Thurs')
+elif(day == 6):
+    print('Fri')
+elif(day == 7):
+    print('Sat')
+else:
+    print('nothing')    
--- if conditions.py
+++
@@ -1,6 +0,0 @@
-username =  input(" plase insert your name: ")
-password = input(" plase insert your password: ")
-if username == 'bohen' and password == 'ffff':
-    print("welcome to your  emaile")
-else:
-    print ('error')
