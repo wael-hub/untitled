d0a8c93d18563d2a032cfc636e36573178372420
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-31 11:16:32 +0300

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,11 @@
+num_of_tries= = 0
+
+while num_of_trise !=3:
+      username = input('plase insert your username:  ')
+      password = input ('insert your password: ')
+      if username == bohen and password == ffff
+         print ("welcome")
+      else:
+         print ("error")
+         num_of_trise + =1
+         print ("you have" + str(3 - num_of_trise) + "trise left ")
