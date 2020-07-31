9596dbe0488c87a3cf5aba1ad4bd4e464839bc7d
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-31 19:36:20 +0300

    Add files via upload

---
+++ Nested if.py
@@ -0,0 +1,16 @@
+age = 0
+word ='python'
+if(age <= 20):
+    if(word == 'python'):
+        print('i am young')
+    else:
+        print('nothing')
+
+elif(age <= 40):
+    if (word == 'bohen'):
+         print('i am in the middle age')
+    else:
+        print('nothing')
+
+else:
+    print('i am old man')
