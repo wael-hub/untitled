2147cccab0328c39423a64bf34e5c28b86205610
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-31 17:11:50 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+first = 'wael'
+last = 'mohamad'
+print (first , last )
+
+fullname = first + ' ' + last
+print (fullname)
+
+str = ('my name is wael')
+print (str)
+
+str1 = 'wwwwwwww'
+print (str1)
+str2 = 5 * 'w'
+print (str2)
