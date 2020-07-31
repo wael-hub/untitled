6893054bf9d4040e45747362f6c6b4e0692d5812
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-07-31 17:50:26 +0300

    Add files via upload

---
+++ indexed.py
@@ -0,0 +1,28 @@
+word = 'bohen'
+print (word)
+print  (word[0])
+print (word[1])
+print (word[2])
+print (word[3])
+print (word[4])
+
+print (word[-1])
+print (word[-2])
+print (word[-3])
+print (word[-4])
+print (word[-5])
+
+print (word[:])
+print (word[3:])
+print (word[3:])
+
+print (word[0:3])
+
+
+word = 'bohen', 'bird' , 10 , 20 , 3.5 ,'python', 'mohamad'
+print(word)
+print  (word[0])
+print  (word[:])
+print  (word[:4])
+
+print  (word[2:5])
