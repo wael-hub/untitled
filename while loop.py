a6e6a2b41c44c6b3c00abb5cfb3fe23c08c9cab1
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-01 12:16:21 +0300

    Update while loop.py

--- while loop.py
+++ while loop.py
@@ -9,3 +9,11 @@ while num_of_trise !=3:
          print ("error")
          num_of_trise + =1
          print ("you have" + str(3 - num_of_trise) + "trise left ")
+      
+      
+      
+      word = 'bohen'
+i = 0
+while i < 10:
+    print(word)
+    i = i + 1
