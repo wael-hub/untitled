aaf17a28326429f682146f31ea88bf89ff6c5e2c
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-01 12:04:10 +0300

    Add files via upload

---
+++ Loop for use break and continue.py
@@ -0,0 +1,16 @@
+names = 'ward', 'bohen' ,'wael' ,'mohamad', 'yazan'
+
+for item in names:
+    if (item == 'wael'):
+        break
+    print(item)
+print('end')
+
+
+
+names = 'ward', 'bohen' ,'wael' ,'mohamad', 'yazan'
+
+for item in names:
+    if (item == 'wael'):
+       continue
+    print(item)
