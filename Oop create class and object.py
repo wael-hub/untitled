8d78b0030c93c096b78ab7c2a831e62a435c3f61
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-03 11:48:32 +0300

    Add files via upload

---
+++ Oop create class and object.py
@@ -0,0 +1,10 @@
+class car:
+    speed = 0
+    clor = 'none'
+    def increment(self):
+        print('increment')
+    def decrement(self):
+        print ('decrement')
+
+BMW = car()
+camry = car()
