184f5bf2691d218c69f331c2cda37be18faaac62
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-05 12:10:41 +0300

    Add files via upload

---
+++ Multi Class.py
@@ -0,0 +1,28 @@
+class A:
+    def printA(self):
+        print('classA')
+
+class B:
+    def printB(self):
+        print('classB')
+
+class C:
+    def printC(self):
+        print('classC')
+
+objA = A()
+objA.printA()
+
+
+objB = B()
+objB.printB()
+
+
+objC = C()
+objC.printC()
+
+
+
+
+
+
