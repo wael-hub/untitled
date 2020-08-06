74d6ad6644cecc503b2e87a5c1bf820e7fd67b3f
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-06 11:38:23 +0300

    Add files via upload

---
+++ Leen and Upper.py
@@ -0,0 +1,21 @@
+name = 'Wael Mohamad'
+res = name.replace('Wael Mohamad', 'Ward')
+print(res)
+
+
+name = 'Wael Mohamad'
+res = name.find('M')
+print(res)
+
+
+name = 'Wael Mohamad'
+res = name.__len__()
+print(res)
+
+
+
+
+name = 'Wael Mohamad'
+res = name.upper()
+print(res)
+
