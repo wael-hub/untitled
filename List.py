2f09705eb8f5be642a16aec2bc53c71e912b9009
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-06 11:55:40 +0300

    Add files via upload

---
+++ List.py
@@ -0,0 +1,12 @@
+numbers = [10,20,30,40,50,60,70,80,90,100]
+print (numbers[7])
+print (numbers[:])
+print (numbers[0:5])
+print (numbers[3:7])
+print (numbers[:8])
+
+
+numbers = [10,20,30,40,50,60,70,80,90,100]
+for item in numbers:
+    print(item)
+
