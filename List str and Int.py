ba97b220b7636ca4a50c08c4dd269933d6fda3a5
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-06 12:05:55 +0300

    Add files via upload

---
+++ List str and Int.py
@@ -0,0 +1,9 @@
+names = ['wael','ward','bohen','younes','yazan']
+for item in names:
+    print(item)
+
+
+variables = [1,2,3,4,'ward','wael','yazan','younes']
+print(variables[:])
+print(variables[3])
+print(variables[-3])
