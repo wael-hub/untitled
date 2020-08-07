e6e3754aa2fbc5e453ea0b4dc54dd8a32a1731cd
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-07 12:11:02 +0300

    Add files via upload

---
+++ Function use in Dictionary.py
@@ -0,0 +1,21 @@
+student = {'id' : 1 ,'name':'wael'}
+student.update({'job':'programmer'})
+print(student)
+
+
+
+student = {'id' : 1 ,'name':'wael'}
+print(student.get('phone','this key not exist'))
+
+
+
+student = {'id' : 1 ,'name':'wael'}
+print(student.__len__())
+
+
+
+student = {'id' : 1 ,'name':'wael'}
+print(student.keys())
+
+
+
