2f17bb13b632fa0ae4d5adf232715e59942c8d4e
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-07 11:26:35 +0300

    Add files via upload

---
+++ Function  using in List.py
@@ -0,0 +1,39 @@
+item = [1,2,4,6,5,7,8,10,9]
+item.sort()
+print(item)
+
+
+item = [1,2,4,6,5,7,8,10,9]
+item.append(20)
+print(item)
+
+
+item = [1,2,4,6,5,7,8,10,9]
+item.clear()
+print(item)
+
+
+item = [1,2,4,6,5,7,8,10,9]
+item.remove(10)
+print(item)
+
+
+
+item = [1,2,4,6,5,7,8,10,9]
+res = item.index(5)
+print(res)
+
+
+item = [1,2,4,6,5,7,8,10,9]
+item.sort()
+print(item)
+
+
+item = [1,2,4,6,5,7,8,10,9]
+item.copy()
+print(item)
+
+
+
+
+
