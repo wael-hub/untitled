70250528863beeb061743f0faf05844c5b58ad89
wael-hub <67709606+wael-hub@users.noreply.github.com>
2020-08-09 13:02:16 +0300

    Add files via upload

---
+++ Import random.py
@@ -0,0 +1,17 @@
+import random
+value = random.random()
+print(value)
+
+
+import random
+value = random.uniform(1,10)
+print(value)
+
+
+import random
+names = ['wael','mohamad','youns']
+value = random.choice(names)
+print(value)
+
+
+
